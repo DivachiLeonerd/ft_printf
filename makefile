a8bde56210517b57748a067a3748ff75821a6554
DivachiLeonerd <81565090+DivachiLeonerd@users.noreply.github.com>
2022-02-15 11:05:39 +0000

    makefile and tester changed

--- makefile
+++ makefile
@@ -4,7 +4,7 @@ CFLAGS := -I. -I./libft/ -Werror -Wextra -Wall
 OBJPATH := ./Objetos/
 OBJS = ft_printf.o print_hexa.o print_integer.o print_unsignedchar.o print_pointer.o
 LIB := ar rcs
-OBJS2 = ft_putchar_fd.o ft_itoa.o ft_putstr_fd.o ft_calloc.o ft_strdup.o ft_strlen.o print_pointer.o
+OBJS2 = ft_putchar_fd.o ft_itoa.o ft_putstr_fd.o ft_calloc.o ft_strdup.o ft_strlen.o
 
 .PHONY: all clean fclean re test mv_objs2
 
@@ -17,10 +17,10 @@ all: ${NAME}
 	@${LIB} ${NAME} ${OBJS} ${OBJS2}
 	@mv *.o ${OBJPATH}
 
-test: ${OBJS} mv_objs2 ${OBJS2}
+test: ${OBJS} mv_o2 ${OBJS2}
 	@${CC} ${CFLAGS} -o tester printf_tester.c ${OBJS} ${OBJS2}
 	@./tester
-mv_objs2:
+mv_o2:
 	@${MAKE} -C ./libft/
 	@mv ./libft/*.o ./
 
--- printf_tester.c
+++ printf_tester.c
@@ -6,58 +6,11 @@
 /*   By: afonso <afonso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 12:49:44 by afonso            #+#    #+#             */
-/*   Updated: 2022/02/13 12:38:10 by afonso           ###   ########.fr       */
+/*   Updated: 2022/02/15 11:02:36 by afonso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"ft_printf.h"
-
-// static	void	deci_hexa(char	*string, char format)
-// {
-// 	unsigned int	i;
-
-// 	i = 0;
-// 	while (string[i])
-// 	{
-// 		if (string[i] < 10)
-// 			string[i] = string[i] + 48;
-// 		if (format == 'p' || format == 'x')
-// 			if (string[i] >= 10)
-// 				string[i] = string[i] + 87;
-// 		if (format == 'X')
-// 			if (string[i] >= 10)
-// 				string[i] = string[i] + 55;
-// 		i++;
-// 	}
-// 	ft_putstr_fd(string, 1);
-// }
-
-// void	print_hexa(unsigned int x, char format)
-// {
-// 	char			string[9];
-// 	unsigned int	i;
-// 	unsigned int	strlen;
-
-// 	string[8] = '\0';
-// 	i = 0;
-// 	i = x;
-// 	while (i)
-// 	{
-// 		i = i / 16;
-// 		strlen++;
-// 	}
-// 	i = strlen;
-// 	while (string[strlen])
-// 		string[strlen++] = '\0';
-// 	while (i >= 0)
-// 	{
-// 		string[i--] = x % 16;
-// 		x = x / 16;
-// 	}
-// 	if (format == 'p')
-// 		ft_putstr_fd("0x", 1);
-// 	deci_hexa(string, format);
-// }
 
 int	main(void)
 {
@@ -66,8 +19,11 @@ int	main(void)
 	unsigned int	u = 1000000;
 	unsigned int	hexalow = 48600;
 	char			c = 'F';
+	int x;
+	int y;
 
-	// print_hexa(u, 'p');
-	printf("Printf normal:\n%d %i %u %s agora %c %x %X %p %%\nAhora soy guapo\n", decimal, integer, u, "string", c, hexalow, hexalow, &hexalow);
-	ft_printf("Print anormal:\n%d %i %u %s agora %c %x %X %p %%\nAhora soy guapo\n", decimal, integer, u, "string", c, hexalow, hexalow, &hexalow);
+	x = printf("Printf normal:\n%d %i %u %s agora %c %x %X %p %%\nAhora soy guapo\n", decimal, integer, u, "string", c, hexalow, hexalow, &hexalow);
+	y = ft_printf("Print anormal:\n%d %i %u %s agora %c %x %X %p %%\nAhora soy guapo\n", decimal, integer, u, "string", c, hexalow, hexalow, &hexalow);
+	printf("%d\n", x);
+	printf("%d\n", y);
 }
