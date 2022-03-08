830fd0a79af8e28c2c6c71a0fd6ee4fac43c7942
Afonso Fernandes <81565090+DivachiLeonerd@users.noreply.github.com>
2022-03-08 12:16:36 +0000

    Update printf_tester.c

--- printf_tester.c
+++ printf_tester.c
@@ -6,7 +6,7 @@
 /*   By: afonso <afonso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 12:49:44 by afonso            #+#    #+#             */
-/*   Updated: 2022/03/04 10:19:17 by afonso           ###   ########.fr       */
+/*   Updated: 2022/03/08 11:47:19 by afonso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,6 @@ int	main(void)
 	mycounter = ft_printf("%p\nbolas\n", (void *)UINT64_MAX);
 	ft_printf("%d\n", mycounter);
 	ft_printf("Tou a 100%%\n");
-	ft_printf("Tou a 100%");
+	printf("Tou a 100%");
 	// printf("Tou a 100%\n");
 }
